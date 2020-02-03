e914fd7593c44c9968ba40f07a57441720b3743d
Anthony Kuske <anthonykuske@gmail.com>
2020-02-03 15:34:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Put these 2 files:
+* autoconfig.js
+* mozilla.cfg
+in the directory:
+
+    C:\Program Files\Mozilla Firefox\defaults\pref
+
+
+For more info see: https://developer.mozilla.org/en-US/docs/Mozilla/Firefox/Enterprise_deployment_before_60#Configuration
---
+++ autoconfig.js
@@ -0,0 +1,3 @@
+// This file tells Firefox to load our settings from mozilla.cfg. Why you do that instead of just putting them here I don't know, but every guide says to do it this way.
+pref("general.config.filename", "mozilla.cfg");
+pref("general.config.obscure_value", 0);
